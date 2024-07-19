f3f7ded6b59a639a0f7d6955fb3e7f58d68884c9
Brent.Voyles3 <Brent.Voyles3@gmail.com>
2024-07-19 12:00:36 -0400

    Added Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,12 @@
 export default function Navbar() {
     return (
-<p>Navbar</p>
+<div className="navbar container"> 
+    <a href="!#" className="logo"> C<span>ha</span>pmanHvac</a>
+    <div className="nav-links">
+        <a href="!#">Home</a>
+        <a href="!#">Services</a>
+        <a href="!#">Gallery</a>
+    </div>
+</div>
     )
 }
