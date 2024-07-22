8fc742cbf25be0e43128291d06f052b1e9dda0bb
Brent Voyles <btv88975@uga.edu>
2024-07-22 10:38:50 -0400

    Edited Sidebar Functionality on Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
+import { useState }  from "react";
 export default function Navbar() {
+    const [showSidebar, setShowSidebar] = useState(false);
     return (
 <div className="navbar container"> 
     <a href="!#" className="logo"> C<span>ha</span>pman<span>H</span>vac</a>
     <div className="nav-links">
-        <a href="!#" className="active">Home</a>
+        <a href="!#">Home</a>
         <a href="!#">Services</a>
         <a href="!#">Gallery</a>
     </div>
-    <div className="sidebar-btn">
+    {/* onClick not working...*/ }
+    <div onClick={() => setShowSidebar(!showSidebar)} className={showSidebar ? "sidebar-btn acative" : "sidebar-btn"}>
         <div className="bar"> </div>
         <div className="bar"> </div>
         <div className="bar"> </div>
