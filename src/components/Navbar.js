6a039aa82d6aed5b9eb4d1546198b0ba14165f6a
Brent Voyles <btv88975@uga.edu>
2024-07-22 11:02:05 -0400

    Edited sidebar dimensions

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,33 @@
 import { useState }  from "react";
+import Sidebar from "./Sidebar";
+
 export default function Navbar() {
     const [showSidebar, setShowSidebar] = useState(false);
+    const links = [
+       {
+        name: "Home",
+        path: "/"
+       },
+       {
+        name: "Services",
+        path: "services"
+       },
+       {
+        name: "Gallery",
+        path: "gallery"
+       },
+    ]
     return (
+<>
 <div className="navbar container"> 
     <a href="!#" className="logo"> C<span>ha</span>pman<span>H</span>vac</a>
     <div className="nav-links">
-        <a href="!#">Home</a>
+        { links.map (link => (
+            <a href="#!" key={link.name}>{link.name}</a>
+        ))}
+       {/* <a href="!#">Home</a>
         <a href="!#">Services</a>
-        <a href="!#">Gallery</a>
+    <a href="!#">Gallery</a> */}
     </div>
     {/* onClick not working...*/ }
     <div onClick={() => {setShowSidebar(!showSidebar)}} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
@@ -16,5 +36,7 @@ export default function Navbar() {
         <div className="bar"> </div>
     </div>
 </div>
+<Sidebar links={links} />
+</>
     )
 }
