c441dffe93fb99c107c04772fd5c7ccb437060a7
Brent.Voyles3 <Brent.Voyles3@gmail.com>
2024-07-23 10:40:17 -0400

    Condensed style, added home page / info section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar"
+import Home from "./components/Home"
 
 function App() {
   return (
     <div className="App">
        <Navbar/>
+       <div className="container main">
+          <Home/>
+       </div>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+export default function Home() {
+    return(
+        <div className="section">
+            <div className="col">
+                <h1 className="title">What We Are About</h1>
+                <p className="info"> ChapmanHVAC is a family owned company that started in 2014. We believe in hardwork, loyalty, and guaranteeing customer satisfaction.</p>
+                <button className="btn"> Explore Now </button>
+            </div>
+            <div className="col">
+                col2
+            </div>
+        </div>
+    )
+}
