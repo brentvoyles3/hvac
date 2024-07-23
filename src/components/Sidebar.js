1cb94647d435c43f8f87352c8f5789d4419441f3
Brent.Voyles3 <Brent.Voyles3@gmail.com>
2024-07-23 10:15:36 -0400

    Added animation and active to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default function Sidebar({links, close}){
     return(
-        <div className="sidebar" onClick={close} >
+        <div className="sidebar" onClick={close}>
         { links.map(link => (
          <a className="sidebar-link" href="#!" key={link.name}>  
                 <FontAwesomeIcon icon={link.icon } />
