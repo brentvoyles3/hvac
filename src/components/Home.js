b43de139a52bdbb1fd2d3e5a6153608500707e90
Brent.Voyles3 <Brent.Voyles3@gmail.com>
2024-07-26 13:43:03 -0400

    Aded HVAC product images to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,21 +2,21 @@ import CustomImage from "./CustomImage"
 
 export default function Home() {
     const images = [
-        "/img/gallery/img_1.jps",
-        "/img/gallery/img_2.jps",
-        "/img/gallery/img_3.jps",
-        "/img/gallery/img_4.jps",
-        "/img/gallery/img_5.jps",
-        "/img/gallery/img_6.jps",
-        "/img/gallery/img_7.jps",
-        "/img/gallery/img_8.jps",
-        "/img/gallery/img_9.jps",
+        "/img/gallery/img_1.jpg",
+        "/img/gallery/img_2.jpg",
+        "/img/gallery/img_3.jpg",
+        "/img/gallery/img_4.jpg",
+        "/img/gallery/img_5.jpg",
+        "/img/gallery/img_6.jpg",
+        "/img/gallery/img_7.jpg",
+        "/img/gallery/img_8.jpg",
+        "/img/gallery/img_9.jpg",
     ]
     return(
         <div className="section home">
             <div className="col">
                 <h1 className="title">What We Are About</h1>
-                <p className="info"> ChapmanHVAC is a family owned company that began business in 2014. We believe in hardwork, loyalty, and guaranteeing customer satisfaction. Don't hesitate to check out more below!</p>
+                <p className="info"> ChapmanHVAC is a family owned company that began business in 2014. We believe in hardwork, loyalty, and guaranteeing customer satisfaction. Don't hesitate to check out more below! We specialize in both residential and commercial installations, repair, and maintenance.</p>
                 <button className="btn"> Explore Now </button>
             </div>
             <div className="col gallery">
