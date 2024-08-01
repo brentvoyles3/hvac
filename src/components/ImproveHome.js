7814a41e40aac0141e9718bb87f43e23db1454b4
Brent Voyles <btv88975@uga.edu>
2024-08-01 09:39:27 -0400

    Added employees image and quote button

--- src/components/ImproveHome.js
+++ src/components/ImproveHome.js
@@ -1,7 +1,16 @@
+import "./Home"
+import "./CustomImage"
+
 export default function ImproveHome() {
     return(
-        <div>
-            Here we are
+        <div className="section improve-home">
+            <div className="col img">
+                <img src="/img/gallery/improve.jpeg" alt=""/>
+            </div>
+        <div className="col">
+            <h1 className="title">Get your HVAC Needs</h1>
+            <button className="btn">Schedule Free Quote</button>
         </div>
+    </div>
     )
 }
